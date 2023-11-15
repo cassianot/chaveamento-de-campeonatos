9478bf998308d77b0bd5fdc502b871f6744e6ecd
Cassiano Tomasini <cassianot@gmail.com>
2023-11-15 11:20:51 -0300

    Auto-generated commit [ci skip]

--- app/model/competidor.model.ts
+++
@@ -1,8 +0,0 @@
-export class Competidor {
-    id? : number;
-    nomeCompetidor: string;
-
-    constructor(nomeCompetidor: string){
-        this.nomeCompetidor = nomeCompetidor;
-    }
-}
--- app/util/web-storage-util.ts
+++
@@ -1,9 +0,0 @@
-export class WebStorageUtil {
-  static get(key: string): any {
-    return JSON.parse(window.localStorage.getItem(key)!);
-  }
-
-  static set(key: string, value: any) {
-    window.localStorage.setItem(key, JSON.stringify(value));
-  }
-}
